1db97282b5f0a8d44bc773df199684c665cc657c
lonelycat <24302010013@m.fudan.edu.cn>
2025-11-06 10:06:49 +0800

    feat: add _delete()

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,25 +1,23 @@
-import type { UserInfo, LoginInfo} from "@/types"
+import type { UserInfo, LoginInfo } from "@/types"
 
 export const userService = {
     register,
-    login
+    login,
+    _delete
 }
 
-async function login(info : LoginInfo){
+async function login(info: LoginInfo) {
     const requestOptions = {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json'},
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(info)
     };
 
-    const response = await fetch (`https://mockurl:4000/users/authenticate`,requestOptions)
+    const response = await fetch(`https://mockurl:4000/users/authenticate`, requestOptions)
     return handleResponse(response);
 }
 
-
-
-
-async function register(user : UserInfo) {
+async function register(user: UserInfo) {
     const requestOptions = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -30,7 +28,22 @@ async function register(user : UserInfo) {
     return handleResponse(response);
 }
 
+async function _delete(id: string) {
+    const requestOptions = {
+        method: 'DELETE',
+        headers: authHeader() //和登录状态相关
+    }
+
+    const response = await fetch(`https://mockurl:4000/users/${id}`,requestOptions);
+    return handleResponse(response);
+}
+
 function handleResponse(response) {
     //check response, if fail, redirect to login / signup
     console.log("handling response")
+}
+
+function authHeader(){
+    //真实场景应当是令牌
+    return { 'Content-Type': 'application/json' }
 }
