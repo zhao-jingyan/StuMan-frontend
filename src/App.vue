f39835d20fcc571ab1ef4f915ce8c61f749dce6a
lonelycat <24302010013@m.fudan.edu.cn>
2025-11-11 17:43:17 +0800

    feat: remove unnecessary dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,4 @@
 <script lang="ts" setup>
-  import SheetManager from './auth/SheetManager.vue';
-  import List from './manage/StudentsList.vue';
-  import type { UserInfo } from '@/types/index';
-
-  const testInfo : UserInfo = {
-    id : "123456",
-    name : "Test Name",
-    nickname : "Try nick",
-    role : "Student",
-    password : "",
-    class : "1",
-  }
 </script>
 
 <template>
